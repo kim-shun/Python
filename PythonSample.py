0d758ac1a0ca1f5ad64fb728f51451e079e35e92
kim-shun <kimura1417080@gmail.com>
2021-09-02 15:56:35 +0900

    Initial commit

---
+++ PythonSample.py
@@ -0,0 +1,25 @@
+a = 2
+b = 3
+c = a + b
+print(c)
+print(a + 5)
+t = "TEXT"
+tt = "text"
+s = "aaaa\nbbbb\tccccdddd" #\nと\t
+print(s,tt)
+s2 = 'aaaabbbb"cccc\'\\dddd' #\の後の文字をそのまま表示させる
+print(s2)
+
+print("c:sample\\")
+print(r"c:sample\\")
+print('C:\some\name')
+print(r'C:\some\name')
+#rで\nをそのまま表示させる
+
+'''
+cccccccc
+cccccccc
+cccccccc
+複数行のコメントアウト
+'''
+
