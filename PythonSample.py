edd6bf13c3fe03340a0919e261cad4f9af9038b0
kim-shun <kimura1417080@gmail.com>
2021-09-03 08:25:26 +0900

    初歩的なコード

--- PythonSample.py
+++ PythonSample.py
@@ -23,3 +23,27 @@ cccccccc
 複数行のコメントアウト
 '''
 
+print(
+    '''
+cccc  cc
+  cccc cc
+cccc  cc
+'''
+    )
+#\nを使わなくとも見た目のまま表示される
+
+print(10/5)
+#2ではなく2.0になる
+print(3 * 'un' + 'ium')
+
+print("""\
+＝＝＝＝＝＝＝＝＝＝＝
+Usage: thingy [OPTIONS]
+ -h
+ -H hostname
+ ＝＝＝＝＝＝＝＝＝＝
+"""
+      )
+#最初の\で改行をなくす
+
+
