c042f1061ed082b498f85a02446fb841fe1a899a
kim-shun <kimura1417080@gmail.com>
2021-09-06 11:52:08 +0900

    別のif文

--- 09:06_python.py
+++ 09:06_python.py
@@ -1,7 +1,22 @@
+'''
 a = int(input("数字を入力してください"))
 if a >= 80:
     print("合格")
 else:
     print("不合格")
 print("処理終了")
+'''
+
+x = int(input("整数を入れてください："))
+if x < 0:
+    x = 0
+    print('負数はゼロとする')
+elif x == 0:
+    print("ゼロ")
+elif x == 1:
+    print("1つ")
+else:
+    print("もっと")
+
+
         
