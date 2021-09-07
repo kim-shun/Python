7e515f7b18d754dbedc17671ac85052ab6dfeec5
kim-shun <kimura1417080@gmail.com>
2021-09-07 17:28:28 +0900

    平均値を出すプログラム

--- 09:07python4.py
+++ 09:07python4.py
@@ -1,21 +1,30 @@
 '''
-i = 5
-
-def f(arg= i):
-    #docstring(関数の説明文)
-    print(arg)
-
-i = 6
-print(i) #6
-f() #5
-#変数のスコープ
-'''
-
-
 def culculate_average(a,b,c,d,e,f,g):
-    average = (a + b + c + d + e + f + g) / 7
+    average = (a + b + c + d + e + f + g) / 7
     return average
+
 
 result = culculate_average(2,3,4,5,6,7,8)
 print("平均は",result,"です")
+'''
+
+def culculate_average(nums, quantity):
+    nums_sum = sum(nums)
+    average = nums_sum / quantity
+    return average
+
+
+quantity = int(input("数字の個数を決めてください："))
+
+nums = []
+
+for i in range(quantity):
+    num = int(input("数字を入力してください："))
+    nums.append(num)
+
+result = culculate_average(nums, quantity)
+print("平均は",result,"です")
+
+
     
+
