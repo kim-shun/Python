6ce1432c7ae705d909a04895c0fd421a7809ad10
kim-shun <kimura1417080@gmail.com>
2021-09-07 14:15:10 +0900

    continueとbreak

--- 09:07_python2.py
+++ 09:07_python2.py
@@ -1,3 +1,4 @@
+'''
 for i in range(5):
     if i == 2:
         #print("True")
@@ -17,3 +18,54 @@ for n in range(2,10):
             break
     else:
         print(n, 'is a prime number')
+
+
+print(8//2) #4
+print(8/2) #4.0
+
+for n in range(2, 10):
+    print(n, "学年")
+    for x in range(2, n):
+        print(x,"組")
+
+for n in range(2, 10):
+    print(n)
+    for x in range(2,n):
+        print(x)
+        if x == 4:
+            #break #5以上でも4でbreak 親のforに抜ける
+            #continue
+
+
+print(2%2) #0
+print(2//2) #1
+
+
+for i in range(2,2):
+    print(i) #何も表示されない
+
+
+for n in range(2,10):
+    print(n)
+else:
+
+
+'''
+
+for n in range(2, 10):
+    print(n) # 2 3 4
+    if n == 4:
+        #print(n) #4
+        break
+    #print(n) #2 3
+else:
+    print("for_end")
+
+fruits = ["apple","banana","cherry","durian"]
+for item in fruits:
+    print(item)
+
+
+
+            
+        
