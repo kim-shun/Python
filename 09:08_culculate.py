ab83ed2badfcd20281e7f08b02cc4145ff06311b
kim-shun <kimura1417080@gmail.com>
2021-09-08 11:13:17 +0900

    とりあえず保存

--- 09:08_culculate.py
+++ 09:08_culculate.py
@@ -2,7 +2,7 @@
 #問題を作成
 #入力してもらう
 #正解かどうかを判定
-
+'''
 def culc(a, b=1, squares=[], cubes=[]):
     print(squares)
     squares.append(a ** 2)
@@ -15,3 +15,19 @@ def culc(a, b=1, squares=[], cubes=[]):
 #print(culc(2, 3)) #([1, 4], [1, 27])
 print(culc(3, 4)) #([1, 4, 9], [1, 27, 64])
 #print(culc(4, 5)) #([1, 4, 9, 16], [1, 27, 64, 125])
+
+a = ['dog','cat','tiger','horse','monkey']
+for s in a[:]:
+	if len(s) == 5:
+		a.append(s)
+print(a)
+'''
+
+'''
+words = ['dog','monkey','cat','horse']
+for i in words:
+    print(len(i))
+    if len(i) >= 5:
+        print(i,end= ",")
+'''
+        
