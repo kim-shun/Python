c500c8c4337db9acdeb2dfefdf2f342087b55721
kim-shun <kimura1417080@gmail.com>
2021-09-08 16:32:37 +0900

    簡単な足し算問題

--- 09:08_random.py
+++ 09:08_random.py
@@ -1,1 +1,23 @@
-#random
+import random
+num1 = random.choice(list(range(100)))
+num2 = random.choice(list(range(100)))
+print(num1, "+", num2,"=")
+while True:
+    answer = int(input("答えを入力してください："))
+    if answer == num1 + num2:
+        print("正解です")
+        break
+    else:
+        print("間違いです")
+
+
+
+
+'''
+s = random.sample(range(100),10)
+print(s)
+r = random.random()
+print(r)
+rr = random.randrange(100)
+'''
+
