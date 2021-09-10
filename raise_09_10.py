4b35e7fcc3ddb535538d283e02ab71085580660a
kim-shun <kimura1417080@gmail.com>
2021-09-11 07:26:59 +0900

    自分でエラーを作る

--- raise_09_10.py
+++ raise_09_10.py
@@ -18,3 +18,58 @@ f(10,5)
 f(10,0)
 f(10,-2) #強制的にエラーにする
 f(-50,9)
+
+#Exception エラーを処理する大親分
+
+#親子の継承
+#CもDもBと同じこともできる
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+#cls()をBCDに入れたから書かなくて良い
+for cls in [B,C,D]:
+    #print(cls) #<class '__main__.B'> <class '__main__.C'> <class '__main__.D'>
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+
+#B
+#C
+#D
+        '''
+    except B:
+        print("B")
+    except C:
+        print("C")
+    except D:
+        print("D")
+
+    の場合
+    B
+    B
+    B
+    になる
+    親子関係の問題
+    全員大親分をExceptionにしたらBBBでなくなる
+    '''
+
+
+
+
+
+
+
+
+
