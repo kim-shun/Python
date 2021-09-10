1073b1a714f5ac6614035b95f7fb428d1c1b5b6a
kim-shun <kimura1417080@gmail.com>
2021-09-10 09:14:06 +0900

    2進数について考えた

--- 09:09_base_number.py
+++ 09:09_base_number.py
@@ -5,13 +5,33 @@ def base_number(num):
         remainders.append(str(remainder))
         num //= 2
         if num == 0:
+            remainders.reverse()
             binary = "".join(remainders)
             print("2進数は" + binary + "です")
             break
         '''
-        2進数の値がString型になっています
+          2進数の値がString型になっています
         '''
 
 num = int(input("数字を入力してください："))
-
 base_number(num)
+
+print()
+print()
+#Pythonの標準機能
+
+num2 = 160
+print(bin(num2))
+#'0b10100000'
+
+num3 = 160
+print(format(num3, 'b'))
+#'10100000'
+
+num4 = 160
+print('{:b}'.format(num4))
+#'10100000'
+
+num5 = 160
+print(f'{num5:b}')
+#'10100000'
---
+++ bin_09:10.py
@@ -0,0 +1,16 @@
+num2 = 160
+print(bin(num2))
+#'0b10100000'
+
+num3 = 160
+print(format(num3, 'b'))
+#'10100000'
+
+num4 = 160
+print('{:b}'.format(num4))
+#'10100000'
+
+num5 = 160
+print(f'{num5:b}')
+#'10100000'
+
