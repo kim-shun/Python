82e5fc431d2618ee724740dd22d5e446d60d5014
kim-shun <kimura1417080@gmail.com>
2021-09-11 14:25:47 +0900

    except内で呼び出すと例外処理は適用されない

--- average_09_10.py
+++ average_09_10.py
@@ -1,5 +1,23 @@
-def a(*b):
-    print(b[0] * 2)
+def culculate_average(nums, quantity):
+    nums_sum = sum(nums)
+    average = nums_sum / quantity
+    return average
 
-b = 1,2,3,4,5,6
-a(b)
+try:
+    def input_num():
+        quantity = int(input("数字の個数を決めてください："))
+        nums = []
+        for i in range(quantity):
+            num = int(input("数字を入力してください："))
+            nums.append(num)
+        result = culculate_average(nums, quantity)
+        print("平均は",result,"です")
+
+    input_num()
+    
+except ZeroDivisionError:
+    print("ゼロはだめよ")
+    input_num()
+except ValueError:
+    print("数字ではありませんよ")
+    input_num()
