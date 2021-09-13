f1c5edabc69be378a346d8ee1fa8a5889fbe6b9e
kim-shun <kimura1417080@gmail.com>
2021-09-13 15:44:31 +0900

    一旦保存

--- 09:13_practice/base_number_mouse_click.py
+++ 09:13_practice/base_number_mouse_click.py
@@ -8,7 +8,8 @@ cv.pack()
 nums = list(range(10))
 
 def btn_click():
-    txt.insert(0,1)
+    for i in range(10):
+        txt.insert(0,i)
 
 # テキストボックス
 txt = Entry(width=20)
@@ -16,15 +17,11 @@ txt.place(x=90, y=100)
 
 # ボタンの作成
 # ボタンの生成・描画
-buttons = []
 i = 30
 for j in range(10):
     button = Button(text=str(nums[j]), command=btn_click)
     cv.create_window(i, 50, win=button)
-    buttons.append(button)
     i += 60
 
-print(buttons[0])
-print(buttons[1])
-print(buttons[2])
+
 
