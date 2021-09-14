ff551f9642f5928bc24d2e0570124338cc6d9a0e
kim-shun <kimura1417080@gmail.com>
2021-09-14 09:12:01 +0900

    入力する値の制限

--- 09:13_practice/hexadecimal.py
+++ 09:13_practice/hexadecimal.py
@@ -1,14 +1,38 @@
+class HexEx(Exception):
+    pass
+
+
 def cal_hexa(num):
-    nums = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+    #nums = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+    nums = '0123456789ABCDEF'
     hexa = ""
     while num:
         remainder = num % 16
-        
         hexa = nums[remainder] + hexa
+        print(hexa)
         num //= 16
     return hexa
 
-print(cal_hexa(160)) #A0
-print(cal_hexa(100)) #64
-print(cal_hexa(50)) #32
-print(cal_hexa(7770)) #1E5A
+try:
+    num = int(input("整数を入力してください："))
+    if num < 0:
+        raise HexEx("マイナスは対象外です")
+    result = cal_hexa(num)
+    print(result)
+except HexEx as e:
+    print(e)
+
+#0だと表示されない
+#負数だと無限ループになる
+
+#Python標準機能の場合
+print(hex(num))
+
+'''
+def cal_hexa2(num2):
+    return (cal_hexa(num // 16) if num >= 16 else '') + '0123456789ABCDEF'[num % 16]
+
+num2 = int(input("整数を入力してください："))
+result2 = cal_hexa(num2)
+print(result2)
+'''
