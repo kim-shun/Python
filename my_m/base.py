fcd23e1615c35785c5d78bd25ac9fa8163abf6b0
kim-shun <kimura1417080@gmail.com>
2021-09-14 13:37:59 +0900

    インポートの仕方で呼び出し方も変わる

--- my_m/base.py
+++ my_m/base.py
@@ -1,5 +1,9 @@
 #base
 
-import my_def
+#import my_def
+#from my_def import *
+from my_def import f3bai,f3kaku
 print("base_script")
-print(my_def.f2bai(3))
+#print(my_def.f2bai(3)) #6
+print(f3bai(4))
+#print(my_def.f3kaku(20,12)) #120.0
