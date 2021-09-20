b0669acee8552fb9bae1e835d0abf41fea0e0948
kim-shun <kimura1417080@gmail.com>
2021-09-20 15:00:29 +0900

    nonlocalは不要？

--- 09:19/btn_click0919.py
+++ 09:19/btn_click0919.py
@@ -6,6 +6,13 @@ def click_count():
     global count
     txt.insert(0,count)
     txt.delete(len(str(count)),END)
+
+    n = 2
+    def double_count():
+        double = count * n
+        txt2.insert(0, double)
+        txt2.delete(len(str(double)),END)
+    double_count()
     count += 1
 
 win = Tk()
@@ -15,6 +22,9 @@ cv.pack()
 
 txt = Entry(width = 3)
 txt.place(x = 130, y = 50)
+
+txt2 = Entry(width = 3)
+txt2.place(x = 160, y = 50)
 
 btn = Button(text='ボタン', command = click_count)
 btn.place(x = 125, y = 100)
