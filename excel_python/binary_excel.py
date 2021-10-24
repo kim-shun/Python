da2efe7e48098437f1cab23c433680596c55c804
kim-shun <kimura1417080@gmail.com>
2021-10-24 09:23:57 +0900

    5つの数字を2進数化

--- excel_python/binary_excel.py
+++ excel_python/binary_excel.py
@@ -4,6 +4,12 @@ book = excel.Workbook()
 sheet = book.active
 
 
+for i in range(5):
+    num = int(input("整数を入力してください:"))
+    sheet.cell(row=i + 2, column=2).value = num
+    sheet.cell(row=i + 2, column=3).value = format(num,'b')
+
+
 num = int(input("整数を入力してください:"))
 
 sheet.cell(row=2, column=2).value = num
