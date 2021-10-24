c17777b8fb01be468ab9ff1454d96a7ca6cda026
kim-shun <kimura1417080@gmail.com>
2021-10-24 20:07:25 +0900

    微修正

--- excel_python/binary_excel.py
+++ excel_python/binary_excel.py
@@ -3,12 +3,17 @@ import openpyxl as excel
 book = excel.Workbook()
 sheet = book.active
 
+sheet["A1"] = "整数"
+sheet["B1"] = "2進数"
+sheet["C1"] = "16進数"
 
-for i in range(5):
+quantity = int(input("入力したい整数の数を決めてください："))
+
+for i in range(quantity):
     num = int(input("整数を入力してください:"))
-    sheet.cell(row=i + 2, column=2).value = num
-    sheet.cell(row=i + 2, column=3).value = format(num,'b')
-    sheet.cell(row=i + 2, column=4).value = hex(num)
+    sheet.cell(row=i + 2, column=1).value = num
+    sheet.cell(row=i + 2, column=2).value = format(num,'b')
+    sheet.cell(row=i + 2, column=3).value = format(num,'x')
 
 
 file_template = "binary_excel.xlsx"
