6abb366f793bdf158150efe9148a907082aa2720
kim-shun <kimura1417080@gmail.com>
2021-10-24 09:27:46 +0900

    16進数変換も追加

--- excel_python/binary_excel.py
+++ excel_python/binary_excel.py
@@ -8,12 +8,7 @@ for i in range(5):
     num = int(input("整数を入力してください:"))
     sheet.cell(row=i + 2, column=2).value = num
     sheet.cell(row=i + 2, column=3).value = format(num,'b')
-
-
-num = int(input("整数を入力してください:"))
-
-sheet.cell(row=2, column=2).value = num
-sheet.cell(row=2, column=3).value = format(num,'b')
+    sheet.cell(row=i + 2, column=4).value = hex(num)
 
 
 file_template = "binary_excel.xlsx"
