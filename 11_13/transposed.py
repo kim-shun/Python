5d4935b1679b0df7941296cf3c6fd47deeb512be
kim-shun <kimura1417080@gmail.com>
2021-11-13 10:55:19 +0900

    matrixの復習

---
+++ 11_13/transposed.py
@@ -0,0 +1,13 @@
+
+matrix = [
+     [1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12],
+    ]
+
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+
+print(transposed)
+#[[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
