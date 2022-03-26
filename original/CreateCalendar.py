6246ed5d30d89e9e01df40e6b794111178087526
kim-shun <kimura1417080@gmail.com>
2022-03-26 15:34:30 +0900

    一旦完成

--- original/CreateCalendar.py
+++ original/CreateCalendar.py
@@ -1,7 +1,8 @@
 import datetime
+import calendar
 
 def create_calendar(date):
-        
+
     month_name = ["","January","February","March","April","May","June",
             "July","August","September","October","November","December"]
 
@@ -9,11 +10,14 @@ def create_calendar(date):
 
     print("Su Mo Tu We Th Fr Sa")
 
-    first_day_week = datetime.date(date.year,date.month,1).weekday() + 1
-    print("%2s " % ' ' * first_day_week,end='')
-    for i in range(1,32):
+    week_num = [1,2,3,4,5,6,0]
+    first_day_week = datetime.date(date.year,date.month,1).weekday()
+    _, lastday = calendar.monthrange(date.year, date.month)
+
+    print("%2s " % ' ' * week_num[first_day_week],end='')
+    for i in range(1,lastday + 1):
         print("%2d " % i,end='')
-        if (first_day_week + i) % 7 == 0:
+        if (week_num[first_day_week] + i) % 7 == 0:
             print()
     print('\n')
 
@@ -27,4 +31,10 @@ may = datetime.date(2022,5,5)
 create_calendar(today)
 create_calendar(april)
 create_calendar(may)
+
+#weekday()メソッドで、月曜日が0で日曜日が6
+#isoweekday()があるが、こちらは月曜日が1で日曜日が7
+#今回日曜日から土曜
+
+
 
--- original/calendar.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-print(datetime.date.today()) #2021-09-11
-
-
