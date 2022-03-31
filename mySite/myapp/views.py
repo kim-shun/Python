e485949aab1dc91968bb1e16d3e8ec39241dd3d0
kim-shun <kimura1417080@gmail.com>
2022-03-31 21:44:55 +0900

    配列を渡す

--- mySite/myapp/views.py
+++ mySite/myapp/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponse
 
 def index_template(request):
     myapp_data = {
-      'app': 'Django'
+      'app': 'Django',
+      'num': range(10),
     }
     return render(request, 'index.html', myapp_data)
