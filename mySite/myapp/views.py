737571e762feb38a2bf786ce79d48f31e75a9d99
kim-shun <kimura1417080@gmail.com>
2022-03-31 21:47:54 +0900

    filterの使用

--- mySite/myapp/views.py
+++ mySite/myapp/views.py
@@ -5,5 +5,6 @@ def index_template(request):
     myapp_data = {
       'app': 'Django',
       'num': range(10),
+      'is_weekday': True,
     }
     return render(request, 'index.html', myapp_data)
