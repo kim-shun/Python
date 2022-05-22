be2c29d437c014d17cf53de0419516eb301ec95d
kim-shun <kimura1417080@gmail.com>
2022-05-22 10:35:08 +0900

    dictの使い方

--- sample/python_test_220518.py
+++ sample/python_test_220518.py
@@ -13,3 +13,25 @@ if a is None:
     print("ない")
 else:
     print("ある")
+
+dict = {}
+
+print(dict)
+if dict is None:
+    print("ぺ")
+else:
+    print("あ")
+
+if len(dict) == 0:
+    print("ない")
+else:
+    print("ある")
+
+dicttest = [{'message':'こんにちは','code':'AA'},{'message':'こんばんは','code':'BB'}]
+print(dicttest)
+
+for dict in dicttest:
+    print(dict)
+    print(dict['message'])
+    
+   
